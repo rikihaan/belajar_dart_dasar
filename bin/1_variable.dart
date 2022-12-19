203913f15010c6e717e6d4e30db16a843d628850
Asep Riki <asepriki2014@gmail.com>
2022-12-19 13:15:10 +0700

    commit pertama

---
+++ bin/1_variable.dart
@@ -0,0 +1,55 @@
+void main(){
+    // Membuat variable
+    String name;
+    name = "Asep Riki";
+
+    print(name);
+    print(name);
+    print(name);
+    print(name);
+
+    // mangubah value variable
+    name = "Maulana Ria Bahrial";
+    print(name);
+
+    // decralasi variable secara langsung
+
+    String firstName = "Riki Asep Hari";
+
+    print(firstName);
+
+    // Kata kunci var untuk membuat variable
+
+    var lastName = "Eko Kurniawan Khannedy";
+    var age = 30;
+
+    print(lastName);    
+
+   
+    var alamat;
+    alamat = "Kp. Siliwangi";
+    print (alamat);
+
+    alamat =209;
+    print(alamat);
+
+    // kata kunci final, agar value di dart tidak bisa di ubah
+
+    final String fullName="Joko Sasmito";
+    print(fullName);
+
+    //coba rubah valuenya
+    //fullName = "Ahmad Suherman";//error can't assign to the final variable
+    // print(fullName);/
+
+    //conts agar variable dan valuenya tidak bisa di ubah, dan nilai nya akan di hardcode saat terjadi compilasi(tidak akan berubah)
+
+    final array1 = [1,2,3];
+    const array2 = [1,2,3];
+
+    array1[0]=5;
+    arrar2[0]=5;
+
+    print(array1);
+    print(array2);
+}
